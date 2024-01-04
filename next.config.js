829f7a2ff3d387a831e0dd3a397a76b9f65e62db
Yuichiro Tsuji <yui.tsuji@mbk-wellness.co.jp>
2024-01-04 07:34:07 +0000

    [Modify] routing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        // /api/*のリクエストをDockerコンテナで起動しているAPIサーバーにプロキシする
+        // host.docker.internalはDockerコンテナからホストのローカルネットワークにアクセスするための特殊なホスト名
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://host.docker.internal:8000/api/:path*',
+            },
+        ]
+    },
+}
 
 module.exports = nextConfig
