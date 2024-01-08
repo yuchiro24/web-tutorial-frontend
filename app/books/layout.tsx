ff0004cfc731a4898b9be1335a2edca0a9f30377
Yuichiro Tsuji <t.yuyu.alies@gmail.com>
2024-01-08 17:29:23 +0900

    [Try] css moduleの適用

--- app/books/layout.tsx
+++ app/books/layout.tsx
@@ -1,15 +1,17 @@
+import styles from './styles.module.css';
+
 // /books 以下で使用する共通レイアウト。Node.jsの決まりにより、ファイル名は layout.tsx とする。
 export default function BooksLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="layout">
-            <header className="header">ヘッダー</header>
-            <div className="container">
-                <aside className="navbar">サイドバー</aside>
-                <main className="content">
+        <div className={styles.layout}>
+            <header className={styles.header}>ヘッダー</header>
+            <div className={styles.container}>
+                <aside className={styles.navbar}>サイドバー</aside>
+                <main className={styles.content}>
                     <section>{children}</section>
                 </main>
             </div>
-            <footer className="footer">フッター</footer>
+            <footer className={styles.footer}>フッター</footer>
         </div>
     );
 }
