4978ae11e73493051596ea7ef25aaf812be77774
Yuichiro Tsuji <t.yuyu.alies@gmail.com>
2024-01-08 18:27:31 +0900

    [Modify] 書籍詳細画面へのMaterialUI導入

--- app/books/[id]/page.tsx
+++ app/books/[id]/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import booksData from "../mock/dummy_books.json";
 import bookDetailsData from "../mock/dummy_book_details.json";
+import { Alert, AlertColor, Snackbar, TableContainer, Typography, Table, TableHead, TableBody, TableRow, TableCell, Button } from "@mui/material";
 
 type Book = {
     id: number
@@ -22,11 +23,24 @@ type BookDetail = {
 }
 
 
-// 本詳細画面
+// 書籍詳細画面
 export default function Page({ params }: { params: { id: number } }) {
     const [book, setBook] = useState<Book | null>(null);
     const [bookDetail, setBookDetail] = useState<BookDetail | null>(null);
     const id = Number(params.id);
+
+    const [isSnackbarOpened, setIsSnackbarOpened] = useState<boolean>(false);
+    const [severity, setSeverity] = useState<AlertColor>('success');
+    const [message, setMessage] = useState<string>('');
+    const result = (serverity: AlertColor, message: string) => {
+        setIsSnackbarOpened(true);
+        setSeverity(serverity);
+        setMessage(message);
+    }
+    const handleQuote = () => {
+        // TODO: 引用数を増やす処理
+        result('success', '引用の登録を行いました');
+    };
 
     useEffect(() => {
         const selectedBook: Book | null = booksData.find((book) => book.id === id) ?? null;
@@ -39,31 +53,37 @@ export default function Page({ params }: { params: { id: number } }) {
 
     return (
         <>
-            <h2>本詳細</h2>
-            <label>タイトル: </label>
-            <span>{book?.title}</span>
-            <table>
-                <thead>
-                    <tr>
-                        <th>タイトル</th>
-                        <th>著者</th>
-                        <th>ジャンル</th>
-                        <th>引用数</th>
-                        <th>登録日</th>
-                        <th>最終更新日</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{bookDetail?.title}</td>
-                        <td>{bookDetail?.author}</td>
-                        <td>{bookDetail?.genre}</td>
-                        <td>{bookDetail?.quotedCount}</td>
-                        <td>{bookDetail?.createdAt}</td>
-                        <td>{bookDetail?.updatedAt}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <Snackbar open={isSnackbarOpened} autoHideDuration={3000} onClose={() => setIsSnackbarOpened(false)}>
+                <Alert severity={severity} sx={{ width: '100%' }}>{message}</Alert>
+            </Snackbar>
+            <Typography variant="h4">本詳細</Typography>
+            <Typography variant="h6">タイトル: {book?.title}</Typography>
+            
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>タイトル</TableCell>
+                        <TableCell>著者</TableCell>
+                        <TableCell>ジャンル</TableCell>
+                        <TableCell>引用数</TableCell>
+                        <TableCell>登録日</TableCell>
+                        <TableCell>最終更新日</TableCell>
+                        <TableCell></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow>
+                        <TableCell>{bookDetail?.title}</TableCell>
+                        <TableCell>{bookDetail?.author}</TableCell>
+                        <TableCell>{bookDetail?.genre}</TableCell>
+                        <TableCell>{bookDetail?.quotedCount}</TableCell>
+                        <TableCell>{bookDetail?.createdAt}</TableCell>
+                        <TableCell>{bookDetail?.updatedAt}</TableCell>
+                        <TableCell><Button variant="contained" type="submit" onClick={() => handleQuote()}>引用</Button></TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+            
         </>
     )
 }
