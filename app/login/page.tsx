281fac0c5964a5de9f908e942f10279f3dc3b440
Yuichiro Tsuji <t.yuyu.alies@gmail.com>
2024-01-09 21:38:54 +0900

    [Modify] ログイン画面

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,53 +1,96 @@
 'use client';
 
-import { Box, Button, Divider, TextField, Typography } from "@mui/material";
+import { Box, Button, CssBaseline, Divider, TextField, ThemeProvider, Typography, createTheme } from "@mui/material";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+
+type FormData = {
+    username: string
+    password: string
+}
 
 // ログイン画面
 export default function Page() {
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const router = useRouter();
+    const defaultTheme = createTheme();
 
-    const handleLogin = () => {
+    const handleLogin = (data: FormData) => {
         // TODO: ログイン処理
         console.log("login");
+        router.push("/books")
     }
+
+    const onSubmit = (event: any): void => {
+        const data: FormData = {
+            username: event.username,
+            password: event.password
+        };
+        handleLogin(data);
+    }
+
     return (
-        <>
-            <Typography variant="h3">ログイン</Typography>
-            <Typography variant="h6">ログインする</Typography>
-            <Box component="form" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <TextField type="email" id="outlined-basic" label="メールアドレス" variant="outlined" required
-                    {...register("email", {
-                        required: {
-                            value: true,
-                            message: "メールアドレスは必須です",
-                        },
-                        pattern: {
-                            value: /^[a-zA-Z0-9]+@[a-zA-Z0-9]+$/i,
-                            message: "メールアドレスの形式が違います"
-                        }
-                    })
-                    }
-                />
-                {errors.email?.message && <Typography variant="body1" color="red">{errors.email.message as String}</Typography>}
-                <Divider sx={{ margin: '10px' }} orientation="horizontal" variant="middle"/>
-                <TextField type="password" id="outlined-basic" label="パスワード" variant="outlined" required
-                    {...register("password", {
-                        required: {
-                            value: true,
-                            message: "パスワードは必須です",
-                        },
-                        minLength: {
-                            value: 8,
-                            message: "パスワードは8文字以上です"
-                        }
-                    })
-                    }
-                />
-                {errors.password?.message && <Typography variant="body1" color="red">{errors.password.message as String}</Typography>}
-                <Divider sx={{ margin: '10px' }} orientation="horizontal" variant="middle"/>
-                <Button variant="contained" onClick={() => handleLogin()} type="submit">ログイン</Button>
-            </Box>
-        </>
-    )
+        <ThemeProvider theme={defaultTheme}>
+            {/* <Container component="main"> */}
+                <CssBaseline />
+                <Box
+                    sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 8, }}
+                >
+                    <Typography component="h1" variant="h5">ログイン</Typography>
+                    <Box component="form" onSubmit={handleSubmit(onSubmit)}>
+                        <TextField
+                            type="text"
+                            id="username"
+                            label="ユーザー名"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            {...register("username", {
+                                required: {
+                                    value: true,
+                                    message: "ユーザー名は必須です",
+                                },
+                                minLength: {
+                                    value: 4,
+                                    message: "ユーザー名は4文字以上で入力してください"
+                                },
+                            })
+                            }
+                        />
+                        {errors.username?.message && <Typography variant="body1" color="red">{errors.username.message as String}</Typography>}
+                    
+                        <TextField
+                            type="password"
+                            id="password"
+                            label="パスワード"
+                            autoComplete="current-password"
+                            fullWidth
+                            margin="normal"
+                            variant="outlined"
+                            {...register("password", {
+                                required: {
+                                    value: true,
+                                    message: "パスワードは必須です",
+                                },
+                                minLength: {
+                                    value: 8,
+                                    message: "パスワードは8文字以上です"
+                                },
+                            })
+                            }
+                        />
+                        {errors.password?.message && <Typography variant="body1" color="red">{errors.password.message as String}</Typography>}
+                        <Button
+                            variant="contained"
+                            type="submit"
+                            fullWidth
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            ログイン
+                        </Button>
+                    </Box>
+                </Box>
+            {/* </Container> */}
+        </ThemeProvider>
+    );
 }
